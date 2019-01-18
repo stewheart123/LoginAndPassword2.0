944a97717744d538afa6ee5a9756dbec2614aba7
stewart tuckwood <stew_62@hotmail.com>
2019-01-18 10:09:00 +0000

    Add project files.

---
+++ LoginAndPassword2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginAndPassword2.Startup))]
+namespace LoginAndPassword2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
